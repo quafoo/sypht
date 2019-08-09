fe91f8e35ef26442f54ddc7b2490e0b1c576aa59
quafoo <quafoochan@gmail.com>
2019-08-09 15:36:00 +1000

    add document page

--- src/pages/documents/index.jsx
+++ src/pages/documents/index.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
-// import styles from './index.scss';
+import { Card, Table } from 'antd';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class Documents extends React.Component {
+  componentWillMount() {
+    const data = [
+      {
+        document: 'https://apod.nasa.gov/apod/image/1908/5D4_5485seeley.jpg',
+        filename: 'IMG3053.jpeg',
+        id: 'ee406c43-5d5f-4720-a750-e5fb6a705851',
+        uploaded: '2019-08-08',
+      },
+      {
+        document: 'https://apod.nasa.gov/apod/image/1908/5D4_5485seeley.jpg',
+        filename: 'IMG3053.jpeg',
+        id: 'ee406c43-5d5f-4720-a750-e5fb6a705851',
+        uploaded: '2019-08-08',
+      },
+    ];
+    this.setState({ dataSource: data });
+  }
+
   render() {
+    const columns = [
+      {
+        title: 'Document',
+        dataIndex: 'document',
+        render: pic => <img src={pic} alt="" style={{ width: '200px', height: '80px' }} />,
+      },
+      {
+        title: 'Filename',
+        dataIndex: 'filename',
+      },
+      {
+        title: 'ID',
+        dataIndex: 'id',
+      },
+      {
+        title: 'Uploaded',
+        dataIndex: 'uploaded',
+      },
+    ];
+    const { dataSource } = this.state;
+    // eslint-disable-next-line no-console
+    console.log(dataSource);
+
     return (
       <div>
-        this is Documents page.
+        <Card title="Documents">
+          <Table
+            bordered
+            columns={columns}
+            dataSource={dataSource}
+          />
+        </Card>
       </div>
     );
   }
