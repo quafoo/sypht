4a1ebb321b305c84ba91f5c4150adf5b2ad6d27a
quafoo <quafoochan@gmail.com>
2019-08-09 13:36:50 +1000

    add upload to predict page

--- src/pages/predict/index.jsx
+++ src/pages/predict/index.jsx
@@ -1,13 +1,66 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-// import styles from './index.scss';
+import { Upload, Icon, message } from 'antd';
+import styles from './index.scss';
 
-// eslint-disable-next-line react/prefer-stateless-function
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!');
+  }
+  return isJpgOrPng && isLt2M;
+}
+
 export default class Predict extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(info) {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, imageUrl => this.setState({ imageUrl, loading: false }));
+    }
+  }
+
   render() {
-    return (
+    const uploadButton = (
       <div>
-        this is Predict page.
+        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <div className="ant-upload-text">Upload</div>
       </div>
+    );
+    const { imageUrl } = this.state;
+    return (
+      <Upload
+        name="document"
+        listType="picture-card"
+        className={styles.doc_uploader}
+        showUploadList={false}
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        beforeUpload={beforeUpload}
+        onChange={this.handleChange}
+      >
+        {imageUrl ? <img src={imageUrl} alt="document" style={{ width: '100%' }} /> : uploadButton}
+      </Upload>
     );
   }
 }
