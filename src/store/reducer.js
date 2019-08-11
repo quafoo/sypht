de9e744001f25dd3c5c02e38ec215d11469d3b40
quafoo <quafoochan@gmail.com>
2019-08-11 18:43:39 +1000

    combine home reducer and predict reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux-immutable';
+import { reducer as homeReducer } from '../pages/home/store';
+import { reducer as predictReducer } from '../pages/predict/store';
 
-const reducer = combineReducers({ });
+const reducer = combineReducers({
+  home: homeReducer,
+  predict: predictReducer,
+});
 
 export default reducer;
