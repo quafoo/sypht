0ac49fa2431c0678147de2e1efb1426cb0eb98a4
quafoo <quafoochan@gmail.com>
2019-08-11 18:45:14 +1000

    connect components in Router with store

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,36 +5,40 @@ import {
   Switch,
   Redirect,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import Admin from './Admin';
 import Login from './pages/login';
-// import Home from './pages/home';
+import Home from './pages/home';
 import Predict from './pages/predict';
 import Documents from './pages/documents';
 import NotFound from './pages/notfound';
+import store from './store';
 
 const Router = () => (
-  <HashRouter>
-    <App>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route
-          path="/"
-          render={() => (
-            <Admin>
-              <Switch>
-                {/* <Route path="/home" component={Home} /> */}
-                <Route path="/predict" component={Predict} />
-                <Route path="/documents" component={Documents} />
-                <Redirect to="/documents" />
-                <Route component={NotFound} />
-              </Switch>
-            </Admin>
-          )}
-        />
-      </Switch>
-    </App>
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route
+            path="/"
+            render={() => (
+              <Admin>
+                <Switch>
+                  <Route path="/home" component={Home} />
+                  <Route path="/predict" component={Predict} />
+                  <Route path="/documents" component={Documents} />
+                  <Redirect to="/home" />
+                  <Route component={NotFound} />
+                </Switch>
+              </Admin>
+            )}
+          />
+        </Switch>
+      </App>
+    </HashRouter>
+  </Provider>
 );
 
 export default Router;
