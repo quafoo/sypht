7fbbaf53ba87430d469a5c60158d9f7e6ce734b0
quafoo <quafoochan@gmail.com>
2019-08-11 18:47:23 +1000

    connect home component to store

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
-import styles from './index.scss';
+import { connect } from 'react-redux';
+import { Card, Table } from 'antd';
+import { actionCreators } from './store';
 
-const Home = () => (
-  <div className={styles.home_wrapper}>
-    Welcome to Sypht!
-  </div>
-);
+class Home extends React.PureComponent {
+  componentDidMount() {
+    this.props.credentials.forEach((item) => {
+      if (item.accessToken === '-') {
+        this.props.updateAccessToken(item.clientId, item.clientSecret);
+      }
+    });
+  }
 
-export default Home;
+  render() {
+    const columns = [
+      {
+        title: 'Field Set',
+        dataIndex: 'fieldSet',
+        key: 'fieldSet',
+      },
+      {
+        title: 'Client ID',
+        dataIndex: 'clientId',
+        key: 'clientId',
+      },
+      {
+        title: 'Client Secret',
+        dataIndex: 'clientSecret',
+        key: 'clientSecret',
+      },
+      {
+        title: 'Access Token',
+        dataIndex: 'accessToken',
+        key: 'accessToken',
+      },
+    ];
+
+    return (
+      <div>
+        <Card title="My Credentials">
+          <Table
+            bordered
+            pagination={{ hideOnSinglePage: true }}
+            columns={columns}
+            dataSource={this.props.credentials}
+          />
+        </Card>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  credentials: state.getIn(['home', 'credentials']).toJS(),
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateAccessToken(clientId, clientSecret) {
+    dispatch(actionCreators.getAccessToken(clientId, clientSecret));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
